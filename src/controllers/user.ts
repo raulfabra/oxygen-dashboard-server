87b50a69cb0e539aa8b672342dbb548a69f10ea1
Raúl Fabra <fabraraul@yahoo.com>
2024-10-07 16:32:00 +0200

    User Controller Done

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -17,7 +17,7 @@ usersController.get("/", async (req: Request, res: Response) => {
   }
 });
 
-usersController.get(":id", async (req: Request<{ id: number }>, res: Response) => {
+usersController.get("/:id", async (req: Request<{ id: number }>, res: Response) => {
   const { id } = req.params;
 
   // Buscamos la habitación por su id
@@ -31,15 +31,64 @@ usersController.get(":id", async (req: Request<{ id: number }>, res: Response) =
 });
 
 usersController.post("/", async (req: Request<{}, {}, User>, res: Response) => {
-  // Save a user
+  const { body } = req;
+
+  // Obtener todos los ids de roomsData y calcular el máximo
+  const ids = usersData.map((user) => user["id"]);
+  const maxId = Math.max(...ids);
+
+  // Crear un nuevo room con un id incrementado
+  const newUser = { ...body, id: maxId + 1, startDate: body.startDate.toDateString() };
+
+  // Agregar la nueva habitación a roomsData
+  usersData.push(newUser);
+
+  // Enviar la respuesta con el nuevo array
+  res.status(201).json([...usersData, newUser]);
 });
 
 usersController.put("/:id", async (req: Request<{ id: number }, {}, User>, res: Response) => {
-  // Update a user by id
+  const { id } = req.params;
+  const { body } = req;
+
+  // Encontramos el índice de la booking por su id
+  const userByIndex = usersData.findIndex((user) => user["id"] === Number(id));
+
+  // Si encontramos la booking, la actualizamos
+  if (userByIndex !== -1) {
+    const updatedUser = {
+      ...usersData[userByIndex],
+      ...body,
+      startDate: body.startDate.toDateString(),
+    };
+
+    // Actualizamos el array de bookings con el objeto actualizado
+    usersData[userByIndex] = updatedUser;
+
+    // Enviamos la respuesta con la booking actualizada
+    res.status(200).json(updatedUser);
+  }
+
+  // Si no encontramos la booking, enviamos una respuesta 404
+  res.status(404).json({ message: "booking not found" });
 });
 
 usersController.delete("/:id", async (req: Request<{ id: number }>, res: Response) => {
-  // Delete a user by id
+  const { id } = req.params;
+
+  // Encontramos el índice de la booking por su id
+  const userByIndex = usersData.findIndex((user) => user["id"] === Number(id));
+
+  // Si la booking existe, la eliminamos
+  if (userByIndex !== -1) {
+    // Eliminamos la booking del array
+    const deletedUser = usersData.splice(userByIndex, 1); // splice devuelve un array con los elementos eliminados
+
+    res.status(200).json({ message: "booking deleted successfully", deletedUser });
+  }
+
+  // Si no encontramos la booking, enviamos una respuesta 404
+  res.status(404).json({ message: "booking not found" });
 });
 
 export default usersController;
